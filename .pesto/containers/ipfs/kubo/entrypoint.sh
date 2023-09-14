a43d428d127974b425ea9bf12f09e74e0c7679b3
Jean-Baptiste Lasselle <jean.baptiste.lasselle@gmail.com>
2023-09-15 01:00:07 +0200

    external acces of ipfs gateway

--- .pesto/containers/ipfs/kubo/entrypoint.sh
+++ .pesto/containers/ipfs/kubo/entrypoint.sh
@@ -12,8 +12,13 @@ echo ''
 echo ''
 echo "Configuring CORS of IPFS/KUBO server"
 echo ''
+# ---
+# https://docs.ipfs.tech/reference/kubo/cli/#ipfs-daemon
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["http://onedev.pokus.io:5001", "http://localhost:3000", "http://127.0.0.1:5001", "https://webui.ipfs.io"]'
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["GET", "PATCH", "DELETE", "PUT", "POST"]'
+ipfs config --json Gateway 'http://onedev.pokus.io:5001'
+# ipfs config --json API.HTTPHeaders.X-Special-Header "[\"so special :)\"]"
+# ipfs config --json Gateway.HTTPHeaders.X-Special-Header "[\"so special :)\"]"
 echo ''
 echo ' Checking IPFS config file'
 echo " \$IPFS_PATH/config = [$IPFS_PATH/config]"
@@ -24,6 +29,11 @@ echo "Content of [$IPFS_PATH/config] of IPFS/KUBO server : "
 echo ''
 echo '# ------------------------------------------------------ # '
 echo ''
+echo ''
+echo ' Checking IPFS config [ipfs config show]'
+echo ''
+ipfs config show
+echo '# ------------------------------------------------------ # '
 # chmod a+rw /data/ipfs/config
 # chmod a+rw -R /data/ipfs/
 chmod a+rw -R $IPFS_PATH
