6f291c80d5a92b4fae2d5f15e1a176adcd0a6c93
Jean-Baptiste Lasselle <jean.baptiste.lasselle@gmail.com>
2023-09-14 23:17:11 +0200

    custom ipfs/kubo docker image

--- .pesto/containers/ipfs/kubo/entrypoint.sh
+++ .pesto/containers/ipfs/kubo/entrypoint.sh
@@ -4,6 +4,12 @@
 # 1. There's an fs-repo, and initializes one if there isn't.
 # 2. The API and Gateway are accessible from outside the container.
 # ENTRYPOINT ["/sbin/tini", "--", "/usr/local/bin/start_ipfs"]
+echo ''
+echo ''
+echo "Configuring CORS of IPFS/KUBO server"
+echo ''
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["http://onedev.pokus.io:5001", "http://localhost:3000", "http://127.0.0.1:5001", "https://webui.ipfs.io"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST"]'
 
 echo ''
 echo ''
