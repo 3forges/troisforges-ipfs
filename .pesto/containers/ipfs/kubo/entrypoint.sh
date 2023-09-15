ddd5cfca8117253efc6ed59c0e5eff58c1a3a12e
Jean-Baptiste Lasselle <jean.baptiste.lasselle@gmail.com>
2023-09-15 02:44:50 +0200

    external acces of ipfs gateway

--- .pesto/containers/ipfs/kubo/entrypoint.sh
+++ .pesto/containers/ipfs/kubo/entrypoint.sh
@@ -47,18 +47,6 @@ echo ''
 ipfs config show
 echo '# ------------------------------------------------------ # '
 
-echo '# ------------------------------------------------------ # '
-echo ''
-echo ''
-echo ' Mime Types for Nginx Conf'
-echo ''
-cat <<EOF >./mime.addons.types
-types {
-    text/plain     txt, js, mjs;
-}
-EOF
-cp ./mime.addons.types /etc/nginx/mime.addons.types
-echo '# ------------------------------------------------------ # '
 # --- 
 # https://github.com/ipfs/kubo/blob/master/docs/environment-variables.md#ipfs_http_routers
 # -
